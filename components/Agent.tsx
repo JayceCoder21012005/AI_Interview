de00641902e1298e5a181a3bd65a2e2f37a3cf80
Jayce <pltbao23@clc.fitus.edu.vn>
2025-04-09 14:28:37 +0700

    implement interview session

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from "react";
 import { vapi } from "@/lib/vapi.sdk";
+import { interviewer } from "@/constants";
 
 enum CallStatus {
     INACTIVE = 'INACTIVE',
@@ -18,7 +19,7 @@ interface SavedMessage {
     content: string;
 }
 
-const Agent = ({ userName, userId, type }: AgentProps) => {
+const Agent = ({ userName, userId, type, interviewId, questions }: AgentProps) => {
 
     const router = useRouter();
     const [isSpeaking, setIsSpeaking] = useState(false);
@@ -27,8 +28,13 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
 
     
     useEffect(() => {
-        const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
-        const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
+        const onCallStart = () => {
+            setCallStatus(CallStatus.ACTIVE);
+        }
+
+        const onCallEnd = () => {
+            setCallStatus(CallStatus.FINISHED)
+        };
 
         const onMessage = (message: Message) => {
             if (message.type === "transcript" && message.transcriptType === "final") {
@@ -60,19 +66,60 @@ const Agent = ({ userName, userId, type }: AgentProps) => {
 
     }, []);
 
+    const handleGenerateFeedback = async (messages: SavedMessage[]) => {
+        console.log('Generate feedback here.');
+
+        const { success, id } = {
+            success: true,
+            id: 'feedback-id'
+        }
+
+        if (success && id) {
+            router.push(`/interview/${interviewId}/feedback`);
+        } else {
+            console.log('Error saving feedback');
+            router.push('/');
+        }
+    }
+
     useEffect(() => {
-        if (callStatus === CallStatus.FINISHED) router.push('/');
+        if (callStatus === CallStatus.FINISHED) {
+            if (type === 'generate') {
+                router.push('/');
+            } else {
+                handleGenerateFeedback(messages);
+            }
+        }
+        // if (callStatus === CallStatus.FINISHED) router.push('/');
     }, [messages, callStatus, type, userId, router]);
 
     const handleCall = async () => {
         setCallStatus(CallStatus.CONNECTING);
 
-        await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
-            variableValues: {
-                username: userName,
-                userid: userId,
+        if (type === 'generate') {
+
+            await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+                variableValues: {
+                    username: userName,
+                    userid: userId,
+                }
+            });
+
+        } else {
+            let formattedQuestions = '';
+            
+            if (questions) {
+                formattedQuestions = questions
+                    .map((question) => `- ${question}`)
+                    .join('\n');
             }
-        });
+
+            await vapi.start(interviewer, {
+                variableValues: {
+                    quesitons: formattedQuestions
+                }
+            });
+        }
     }
 
     const handleDisconnect = async () => {
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 import DisplayTechIcons from './DisplayTechIcons';
 
 
-const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt}: 
+const InterviewCard = ({ id, userId, role, type, techstack, createdAt}: 
     InterviewCardProps) => {
     
     const feedback = null as Feedback | null;
@@ -68,8 +68,8 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt}:
                     <Button className="btn-primary">
                         <Link 
                             href={feedback 
-                                ? `/interview/${interviewId}/feedback` 
-                                : `/interview/${interviewId}`}>
+                                ? `/interview/${id}/feedback` 
+                                : `/interview/${id}`}>
 
                             {feedback ? "Check Feedback" : "View Interview"}
 
